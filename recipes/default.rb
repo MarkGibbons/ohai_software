2a0e923c3cb3670e1fb386eb967ce5713697e6cd
markgibbons <mark.gibbons@nordstrom.com>
2015-11-09 11:07:17 -0800

    UIS-766: Fix typoes

--- recipes/default.rb
+++
@@ -1,9 +0,0 @@
-#
-# Recipe: ohai_software::default
-# Copyright (c) 2015 Nordstrom, Inc.
-#
-
-# a common pattern is to seperate the cookbook into distinct
-# recipes and pull them all in via the default recipe
-# include_recipe 'ohai_software::recipe_a'
-# include_recipe 'ohai_software::recipe_b'
